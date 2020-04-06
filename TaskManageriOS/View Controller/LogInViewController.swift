bf99f1fc613fb2492f0e441e39d2cc239777c061
JasonHaque <zahinjason220434@gmail.com>
2020-04-07 01:34:10 +0600

    added log in ib events

--- TaskManageriOS/View Controller/LogInViewController.swift
+++ TaskManageriOS/View Controller/LogInViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var EmailField: UITextField!
+    @IBOutlet weak var PasswordField: UITextField!
+    @IBOutlet weak var ErrorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        ErrorLabel.text = ""
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func LogInTapped(_ sender: Any) {
+        
+        print("log in tapped")
     }
-    */
-
+    
 }
