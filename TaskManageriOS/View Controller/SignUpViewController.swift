09ac3475fc26cb267f84ecb743ae123b083b0c8f
JasonHaque <zahinjason220434@gmail.com>
2020-04-07 03:45:17 +0600

    added verification code

--- TaskManageriOS/View Controller/SignUpViewController.swift
+++ TaskManageriOS/View Controller/SignUpViewController.swift
@@ -24,6 +24,31 @@ class SignUpViewController: UIViewController {
 
     @IBAction func SignUpTapped(_ sender: Any) {
         print("Sign Up Tapped")
+        let email = SignUpEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let passWord = SignUpPassword.text!.trimmingCharacters(in: .newlines)
+        let confirmPassword = ConfirmPassword.text!.trimmingCharacters(in: .newlines)
+        let error = verifyInputs(email,passWord,confirmPassword)
+        if(error != ""){
+            SignUpErrorLabel.text = error
+            return
+        }
+        SignUpErrorLabel.text = ""
+    }
+    
+    func verifyInputs(_ email:String,_ password:String,_ confirmPassowrd:String) -> String {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        if (email == "" || password == "" || confirmPassowrd == "") {
+            return "Fill up the fields properly"
+        }
+        else if(!emailPred.evaluate(with: email)){
+            return "Not an email address"
+        }
+        else if(password != confirmPassowrd){
+            return "Passwords not matching"
+        }
+        return ""
     }
     
 }
