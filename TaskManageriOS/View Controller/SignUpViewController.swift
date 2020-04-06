4384b6b8ed8093468b87298fb4e13d6a6cd0c5fa
JasonHaque <zahinjason220434@gmail.com>
2020-04-07 01:36:57 +0600

    added sign up ib events

--- TaskManageriOS/View Controller/SignUpViewController.swift
+++ TaskManageriOS/View Controller/SignUpViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var SignUpEmail: UITextField!
+    @IBOutlet weak var SignUpPassword: UITextField!
+    
+    @IBOutlet weak var SignUpErrorLabel: UILabel!
+    @IBOutlet weak var ConfirmPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        SignUpErrorLabel.text = ""
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func SignUpTapped(_ sender: Any) {
+        print("Sign Up Tapped")
     }
-    */
-
+    
 }
