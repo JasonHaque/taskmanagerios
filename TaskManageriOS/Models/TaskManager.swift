cb9168c5ba4b15bbdef414f146610ebad4ba5592
JasonHaque <zahinjason220434@gmail.com>
2020-05-15 01:51:35 +0600

    minor changes

--- TaskManageriOS/Models/TaskManager.swift
+++ TaskManageriOS/Models/TaskManager.swift
@@ -7,8 +7,34 @@
 //
 
 import Foundation
+import Firebase
 
-class TaskManager{
+public class TaskManager{
     
-    
+    func getTask()->[TaskModel]{
+        var taskArray = [TaskModel]()
+        let email = (Auth.auth().currentUser?.email)!.split(separator: "@")
+               let user = String(email[0])
+               let dref = Database.database().reference().child("Users").child(user)
+               dref.observe(DataEventType.value, with: {(DataSnapshot) in
+                   if (DataSnapshot.childrenCount > 0){
+                       //self.taskList.removeAll()
+                       for tasks in DataSnapshot.children.allObjects as! [DataSnapshot]{
+                           let taskObject = tasks.value as? [String : AnyObject]
+                           let taskName = taskObject?["taskName"]
+                           let taskDesc = taskObject?["taskDesc"]
+                           let id = taskObject?["Id"]
+                           
+                           let task = TaskModel(id: id as! String?, taskName: taskName as! String?, taskDesc: taskDesc as! String?)
+                           //self.taskList.append(task)
+                        taskArray.append(task)
+                            
+                           
+                       }
+                   }
+                   //print(self.taskList)
+                   //self.Tabletaskview.reloadData()
+               })
+        return taskArray
+    }
 }
