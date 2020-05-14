d4a4f90e930cf2c1f2a67f5c7bf6256c9e2e2eaa
JasonHaque <zahinjason220434@gmail.com>
2020-05-15 01:39:46 +0600

    file structure changed

---
+++ TaskManageriOS/Models/TaskManager.swift
@@ -0,0 +1,14 @@
+//
+//  TaskManager.swift
+//  TaskManageriOS
+//
+//  Created by Sanviraj Zahin Haque on 15/5/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
+
+class TaskManager{
+    
+    
+}
--- TaskManageriOS/views/TableView Cells/TaskTableViewCell.swift
+++ TaskManageriOS/views/TableView Cells/TaskTableViewCell.swift
@@ -14,13 +14,11 @@ class TaskTableViewCell: UITableViewCell {
     @IBOutlet weak var taskDesc: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
