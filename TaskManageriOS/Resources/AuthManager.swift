900bb549473ca4c0152cdd02cf6315b00a329df4
JasonHaque <zahinjason220434@gmail.com>
2020-09-12 00:31:46 +0600

    code

---
+++ TaskManageriOS/Resources/AuthManager.swift
@@ -0,0 +1,9 @@
+//
+//  AuthManager.swift
+//  TaskManageriOS
+//
+//  Created by Sanviraj Zahin Haque on 12/9/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
---
+++ TaskManageriOS/Resources/DatabaseManager.swift
@@ -0,0 +1,9 @@
+//
+//  DatabaseManager.swift
+//  TaskManageriOS
+//
+//  Created by Sanviraj Zahin Haque on 12/9/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
