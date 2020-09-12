0ac6effe7629cc3c7fb8e10e505c4d6e616bccde
JasonHaque <zahinjason220434@gmail.com>
2020-09-12 23:26:40 +0600

    frames added

--- TaskManageriOS/Controllers/Authentication/RegistrationViewController.swift
+++ TaskManageriOS/Controllers/Authentication/RegistrationViewController.swift
@@ -16,6 +16,15 @@ class RegistrationViewController: UIViewController {
         return scrollView
     }()
     
+    private let imageView : UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
     private let UserNameField : UITextField = {
         let field = UITextField()
         field.autocapitalizationType = .none
@@ -85,6 +94,7 @@ class RegistrationViewController: UIViewController {
         
         RegisterButton.addTarget(self, action: #selector(didTapSignUp), for: .touchUpInside)
         
+        scrollView.addSubview(imageView)
         scrollView.addSubview(UserNameField)
         scrollView.addSubview(EmailTextField)
         scrollView.addSubview(passwordField)
@@ -92,6 +102,41 @@ class RegistrationViewController: UIViewController {
         view.addSubview(scrollView)
         
         
+        
+    }
+    
+    //set up frames for views
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        
+        let size = view.width/3
+        
+        imageView.frame = CGRect(x: (view.width - size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+        
+        UserNameField.frame = CGRect(x: 30,
+                                     y: imageView.bottom + 10,
+                                     width: scrollView.width-60,
+                                     height: 52)
+        
+        
+        EmailTextField.frame = CGRect(x: 30,
+                                      y: UserNameField.bottom + 10,
+                                      width: scrollView.width-60,
+                                      height: 52)
+        passwordField.frame = CGRect(x: 30,
+                                     y: EmailTextField.bottom+10,
+                                     width: scrollView.width-60,
+                                     height: 52)
+        RegisterButton.frame = CGRect(x: 30,
+                                      y: passwordField.bottom+10,
+                                      width: scrollView.width-60,
+                                      height: 52)
         
     }
     
