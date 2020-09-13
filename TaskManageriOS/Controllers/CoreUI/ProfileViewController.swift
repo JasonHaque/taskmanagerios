92b6dbf878cf55aee46578b1556761d85eea610c
JasonHaque <zahinjason220434@gmail.com>
2020-09-13 12:39:58 +0600

    profile table added

--- TaskManageriOS/Controllers/CoreUI/ProfileViewController.swift
+++ TaskManageriOS/Controllers/CoreUI/ProfileViewController.swift
@@ -8,17 +8,56 @@
 
 import UIKit
 
+enum ProfileViewModelType{
+    case info , logout
+}
+
+struct ProfileViewModel{
+    
+    let viewModelType : ProfileViewModelType
+    let title : String
+    let handler : (() -> Void)?
+ 
+}
+
 class ProfileViewController: UIViewController {
+    
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(ProfileTableViewCell.self, forCellReuseIdentifier: ProfileTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         title = "Profile"
+        
+        view.addSubview(tableView)
     }
     
 
     
 
+}
+
+extension ProfileViewController : UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    
 }
---
+++ TaskManageriOS/Views/TableCells/ProfileTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileTableViewCell.swift
+//  TaskManageriOS
+//
+//  Created by Sanviraj Zahin Haque on 13/9/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    
+    static let identifier = "ProfileTableViewCell"
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setUp(){
+        
+    }
+    
+    
+    
+}
