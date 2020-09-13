6dd7be55cd404e9a5a74225ba19e22e3eef878b1
JasonHaque <zahinjason220434@gmail.com>
2020-09-13 19:02:58 +0600

    set up views for new task

--- TaskManageriOS/Controllers/NewTaskViewController.swift
+++ TaskManageriOS/Controllers/NewTaskViewController.swift
@@ -10,15 +10,50 @@ import UIKit
 
 class NewTaskViewController: UIViewController {
     
+    private let taskNameField : UITextField = {
+        let textfield = UITextField()
+        textfield.autocorrectionType = .no
+        textfield.autocapitalizationType = .none
+        textfield.placeholder = "Enter the name of the Task"
+        textfield.layer.cornerRadius = 12
+        textfield.layer.borderWidth = 1
+        textfield.returnKeyType = .continue
+        textfield.layer.borderColor = UIColor.lightGray.cgColor
+        textfield.backgroundColor = .secondarySystemBackground
+        textfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        textfield.leftViewMode = .always
+        return textfield
+    }()
     
-    //add necessary views
+    private let taskDescriptionField : UITextView = {
+        
+        let textView = UITextView()
+        
+        return textView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        view.addSubview(taskNameField)
+        view.addSubview(taskDescriptionField)
     }
     
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        taskNameField.frame = CGRect(x: 30,
+                                     y: 100,
+                                     width: view.width - 60,
+                                     height: 52)
+        taskDescriptionField.frame = CGRect(x: 30,
+                                            y: taskNameField.bottom + 20,
+                                            width: view.width - 60,
+                                            height: 52)
+    }
 
    
 
